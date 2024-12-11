27df9fef3a4ba83147fdbb01a1e37db99d599bd4
jun <an0yjun@naver.com>
2024-12-12 00:43:48 +0900

    spring cloud config 설정

--- chat-service-nest/src/app.module.ts
+++ chat-service-nest/src/app.module.ts
@@ -1,18 +1,51 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit, Injectable } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { ChatModule } from '@/chat/chat.module';
-import configuration from '@/config/configuration';
-import { ConfigModule } from '@nestjs/config';
+import yamlConfigLoader from '@/config/yaml.config.loader';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { SpringCloudConfigService } from './config/spring.cloud.config.service';
+import { AxiosResponse } from 'axios';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ChatModule,
     ConfigModule.forRoot({
-      load: [configuration],
+      load: [yamlConfigLoader],
+      isGlobal: true,
+    }),
+    HttpModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (
+        configservice: ConfigService,
+        springCloudConfigService: SpringCloudConfigService,
+      ) => {
+        const springCloudConfig: Record<string, any> =
+          await springCloudConfigService.getConfig();
+        const db_url = springCloudConfig
+          .get('spring.datasource.url')
+          ?.ConfigService.get('');
+
+        return {
+          uri: springCloudConfig.get(''),
+        };
+      },
+      inject: [ConfigService, SpringCloudConfigService],
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SpringCloudConfigService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(
+    private springCloudConfigService: SpringCloudConfigService,
+    private httpService: HttpService,
+  ) {}
+  async onModuleInit() {
+    const config = await this.springCloudConfigService.getConfig();
+    console.log(config);
+  }
+}
