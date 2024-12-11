8ef76f96ef6dd90ab0d566feaf447fce1df7bc08
0jun <an0yjun@gmail.com>
2024-12-11 18:07:32 +0900

    config 설정

--- chat-service-nest/src/app.module.ts
+++ chat-service-nest/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { ChatModule } from '@/chat/chat.module';
+import configuration from '@/config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ChatModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
