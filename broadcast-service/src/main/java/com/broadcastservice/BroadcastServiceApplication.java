76cd21ea56f5786911c2b20f1688dee1b811db2f
0jun <an0yjun@gmail.com>
2024-12-12 15:39:07 +0900

    kafka 설정

--- broadcast-service/src/main/java/com/broadcastservice/BroadcastServiceApplication.java
+++ broadcast-service/src/main/java/com/broadcastservice/BroadcastServiceApplication.java
@@ -2,8 +2,11 @@ package com.broadcastservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BroadcastServiceApplication {
 
     public static void main(String[] args) {
