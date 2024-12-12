3cbb6bb75271108eb52f26d612029491a6c6e57b
0jun <an0yjun@gmail.com>
2024-12-12 14:44:31 +0900

    kafka 등록

--- chat-service-nest/src/main.ts
+++ chat-service-nest/src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { client } from './config/eureka-config';
-import { MicroserviceOptions } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
-    {},
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'chat-consumer',
+        },
+      },
+    },
   );
   await app.listen();
   try {
