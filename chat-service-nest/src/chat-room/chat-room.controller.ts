945d2486047a924a90583802324ff57c1f6862e7
0jun <an0yjun@gmail.com>
2024-12-12 17:27:48 +0900

    kakfa 설정 변경

--- chat-service-nest/src/chat-room/chat-room.controller.ts
+++ chat-service-nest/src/chat-room/chat-room.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Logger } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Controller, Logger, OnModuleInit } from '@nestjs/common';
+import {
+  ClientKafka,
+  EventPattern,
+  MessagePattern,
+  Payload,
+} from '@nestjs/microservices';
 
 @Controller('chat-room')
 export class ChatRoomController {
   private logger = new Logger(ChatRoomController.name);
 
-  @MessagePattern('broadcsast-created')
+  @EventPattern('broadcsast-created')
   handleBroadcastCreated(@Payload() message: any) {
     this.logger.log('reseive broadcast-created', message);
   }
